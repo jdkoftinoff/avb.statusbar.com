14a9d309ca9d5ad8b2c42a390bf48ff7efe2ca46
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-08-04 13:25:31 -0700

    Disable addthis

--- conf.py
+++ conf.py
@@ -351,7 +351,7 @@ MathJax.Hub.Config({
 
 # Enable Addthis social buttons?
 # Defaults to true
-ADD_THIS_BUTTONS = True
+ADD_THIS_BUTTONS = False
 
 # Hide link to source for the posts?
 HIDE_SOURCELINK = True
